4116f7fb355c9534610d3a0e65519d4fcfdaddec
Prabhash Wijerathna <106425954+PrabhaWijera@users.noreply.github.com>
2023-10-12 19:52:31 +0530

    vehical server make

---
+++ Vehicel-Server/src/main/java/com/example/prabhash/vehicelserver/VehicelServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.prabhash.vehicelserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicelServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VehicelServerApplication.class, args);
+    }
+
+}
