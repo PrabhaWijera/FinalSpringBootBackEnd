1bde0edd8ea7a9ffc4d0110cf11ba84cc56d21d5
Prabhash Wijerathna <106425954+PrabhaWijera@users.noreply.github.com>
2023-10-12 21:00:42 +0530

    package detail server make

--- Hotel-Server/src/main/java/com/example/prabhash/hotelserver/HotelServerApplication.java
+++ Hotel-Server/src/main/java/com/example/prabhash/hotelserver/HotelServerApplication.java
@@ -2,8 +2,10 @@ package com.example.prabhash.hotelserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class HotelServerApplication {
 
     public static void main(String[] args) {
---
+++ Package-Details-Server/src/main/java/com/example/prabhash/packagedetailsserver/PackageDetailsServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.prabhash.packagedetailsserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PackageDetailsServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PackageDetailsServerApplication.class, args);
+    }
+
+}
