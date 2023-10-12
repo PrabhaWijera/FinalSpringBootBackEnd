ab2054ab23ebcad08e314e335e437c9aacfe68d3
Prabhash Wijerathna <106425954+PrabhaWijera@users.noreply.github.com>
2023-10-12 20:02:31 +0530

    Hotel server make

--- Vehicel-Server/src/main/java/com/example/prabhash/vehicelserver/VehicelServerApplication.java
+++ Vehicel-Server/src/main/java/com/example/prabhash/vehicelserver/VehicelServerApplication.java
@@ -2,8 +2,10 @@ package com.example.prabhash.vehicelserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class VehicelServerApplication {
 
     public static void main(String[] args) {
