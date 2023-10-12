983fd28e5f3940b40fe7820a9cb4b3d9262d5716
Prabhash Wijerathna <106425954+PrabhaWijera@users.noreply.github.com>
2023-10-13 00:49:42 +0530

    guide entity   make

--- Guide-Server/src/main/java/com/example/prabhash/guideserver/dto/Guide_dto.java
+++ Guide-Server/src/main/java/com/example/prabhash/guideserver/dto/Guide_dto.java
@@ -4,6 +4,33 @@ import java.io.Serializable;
 
 public class Guide_dto implements Serializable {
 
+    private String guideID;
+
+    private String guideName;
+
+    private String guideAddress;
+
+    private String guideAge;
+
+    private String guideGender;
+
+    private String guidePICImage;
+
+    private String guideNICFrontIMG;
+
+    private String guideNICRearIMG;
+
+    private String guideIDscannedFrontIMG;
+    private String guideIDscannedRearIMG;
+
+    private String guideExperience;
+
+    private int manDayValue;
+
+
+    private String remark;
+
+
 
 
 
--- Guide-Server/src/main/java/com/example/prabhash/guideserver/entity/Guide_entity.java
+++ Guide-Server/src/main/java/com/example/prabhash/guideserver/entity/Guide_entity.java
@@ -1,4 +1,47 @@
 package com.example.prabhash.guideserver.entity;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+@Entity
 public class Guide_entity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String guideID;
+
+    private String guideName;
+
+    private String guideAddress;
+
+    private String guideAge;
+
+    private String guideGender;
+
+    private String guidePICImage;
+
+    private String guideNICFrontIMG;
+
+    private String guideNICRearIMG;
+
+    private String guideIDscannedFrontIMG;
+    private String guideIDscannedRearIMG;
+
+    private String guideExperience;
+
+    private int manDayValue;
+
+
+    private String remark;
 }
