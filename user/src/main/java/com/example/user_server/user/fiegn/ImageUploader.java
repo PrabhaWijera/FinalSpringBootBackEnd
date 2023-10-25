a097620cfbd5687864496ed002177d11c3129dd9
Prabhash Wijerathna <106425954+PrabhaWijera@users.noreply.github.com>
2023-10-25 18:04:47 +0530

    2023/10/25 Upload seve creating.....

--- user/src/main/java/com/example/user_server/user/fiegn/ImageUploader.java
+++ user/src/main/java/com/example/user_server/user/fiegn/ImageUploader.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
-@FeignClient("Upload-service")
+@FeignClient("upload-service")
 public interface ImageUploader {
     @PostMapping(path = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public String handleUploads(@RequestParam("imageFile") MultipartFile imageFile);
