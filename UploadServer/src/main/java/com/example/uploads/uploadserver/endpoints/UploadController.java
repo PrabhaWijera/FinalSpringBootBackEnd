9bc6408419228287862773688e6000c84b606d0c
Prabhash Wijerathna <106425954+PrabhaWijera@users.noreply.github.com>
2023-10-26 23:33:04 +0530

    2023/10/26  all sever and upload sever save images path in desktop checking  .....

--- UploadServer/src/main/java/com/example/uploads/uploadserver/endpoints/UploadController.java
+++ UploadServer/src/main/java/com/example/uploads/uploadserver/endpoints/UploadController.java
@@ -9,6 +9,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.ArrayList;
+
 @CrossOrigin
 @RestController
 @RequestMapping("api/v1/uploadingUploader")
@@ -20,6 +22,7 @@ public class UploadController {
     @PostMapping(path = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public String handleUploads(@RequestParam("imageFile") MultipartFile imageFile) {
 
+        System.out.println("Downloaded!!!!");
         return uploadService.handleUploads(imageFile);
 
 
@@ -27,7 +30,7 @@ public class UploadController {
 
     @GetMapping(path = "/getImage",params = "imagePath")
     public ResponseEntity<Resource> getImage(@RequestParam("imagePath") String imagePath) {
-
+        System.out.println("Uploaded!!!!");
         return uploadService.getImage(imagePath);
     }
 
