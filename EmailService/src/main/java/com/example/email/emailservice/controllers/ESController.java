79b7d218884593b6f4291c4cb57a07f70b08b13c
Prabhash Wijerathna <106425954+PrabhaWijera@users.noreply.github.com>
2023-11-02 10:05:32 +0530

    2023/10/31 all finalizing email....

--- EmailService/src/main/java/com/example/email/emailservice/controllers/ESController.java
+++ EmailService/src/main/java/com/example/email/emailservice/controllers/ESController.java
@@ -11,7 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("")
+@RequestMapping("api/v1/emails")
 @CrossOrigin
 public class ESController {
     @Autowired
